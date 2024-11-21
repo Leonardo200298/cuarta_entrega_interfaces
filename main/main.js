81ca22a88d6ee0ee761ee3d73955f91b390593cd
Leonardo200298 <leonardosanchez119@gmail.com>
2024-11-21 14:50:05 -0300

    seccion de las matematicas echa

--- main/main.js
+++ main/main.js
@@ -1,16 +1,28 @@
 "use strict";
 
+/* Variables */
 let header = document.querySelector("#header");
 let menuDeHamburguesa = document.querySelector("#menuDeHamburguesaContenedor");
 let botonComprar = document.querySelector("#botonComprar");
 let imagenLogo = document.querySelector("#imagenLogo");
 let menuDeHamburguesaInterior = document.querySelector("#menuHambNB")
-
-
+let contenedorSeccion3 = document.querySelector("#contenedorSeccion3");
+let imagenSeccion3 = document.querySelector(".fondoDeSeccion3");
+/* Eventos */
 window.addEventListener("scroll", capturaDePixeles);
 menuDeHamburguesa.addEventListener("click", transicionMenuHamburguesa);
+/* direccion opuesta al mousse */
+contenedorSeccion3.addEventListener("mousemove", (e)=>{
+  
+  const x = (e.clientX / window.innerWidth) - 0.5;
+  const y = (e.clientY / window.innerHeight) - 0.5;
+  
+  const speed = imagenSeccion3.getAttribute('data-speed');
+  imagenSeccion3.style.transform = `translate(${x * speed * 100}px, ${y * speed * 100}px)`;
+});
 
 
+/* Scroll */
 function capturaDePixeles() {
   if (window.scrollY > 100) { 
     header.classList.add("headerScroll");
@@ -25,11 +37,16 @@ function capturaDePixeles() {
 
   }
 }
+
+/* menu desplegable */
 function transicionMenuHamburguesa() {
   const menuHamburguesa = document.getElementById("menuHamburguesa");
   const menuLista = document.getElementById("menuLista");
 
-  // Alternar clases para el menú hamburguesa y la lista
+
   menuHamburguesa.classList.toggle("activo");
   menuLista.classList.toggle("activo");
 }
+
+
+
